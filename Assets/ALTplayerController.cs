2c8c82d2e6302d07b6668729eac5e6a54a195eff
sepj <serzh.matusevich@mail.ru>
2024-09-18 15:55:22 +0300

    Alternative(now main) move&shoot system

---
+++ Assets/ALTplayerController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ALTPlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
+    [Range(0.1f, 1f)]
+    [SerializeField] private float fireRate = 0.5f;
+
+    private Rigidbody2D rb;
+    private float mx;
+    private float my;
+
+    private Vector2 mousePos;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+        my = Input.GetAxisRaw("Vertical");
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(mx, my).normalized * speed;
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bulletPrefab, firingPoint.position, firingPoint.rotation);
+    }
+}
